dc2959f813a589726c1fac50c1a584eceb3c387d
Lucas Pietro <lucaspietro.etec@gmail.com>
2024-02-14 17:34:09 -0300

    Lisca ordenada em ordem alfabetica com primeira e ulta palavra

---
+++ python.py
@@ -0,0 +1,13 @@
+palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca']
+
+# Ordenar a lista em ordem alfabética
+palavras_ordenadas = sorted(palavras)
+
+# Primeira palavra na ordem do dicionário
+primeira_palavra = palavras_ordenadas[0]
+
+# Última palavra na ordem do dicionário
+ultima_palavra = palavras_ordenadas[-1]
+
+print("Primeira palavra na ordem do dicionário:", primeira_palavra)
+print("Última palavra na ordem do dicionário:", ultima_palavra)
